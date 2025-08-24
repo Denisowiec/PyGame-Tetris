a64725455d099e574191cbe2f77b35f9dbdf569a
Denisowiec <ddspeed@gmail.com>
2025-08-24 18:23:23 +0200

    Made a cursor object for the player

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame, os
-from block import *
 from playfield import *
 
 def main():
@@ -7,16 +6,28 @@ def main():
     screen = pygame.display.set_mode((1280,720))
     clock = pygame.time.Clock()
 
-    field = Playfield(screen)
-    field.fill_with_blocks()
+    blue_block = pygame.transform.scale2x(pygame.image.load(os.path.join("graphics", "blue.png")))
+    green_block = pygame.transform.scale2x(pygame.image.load(os.path.join("graphics", "green.png")))
+    red_block = pygame.transform.scale2x(pygame.image.load(os.path.join("graphics", "red.png")))
+    violet_block = pygame.transform.scale2x(pygame.image.load(os.path.join("graphics", "violet.png")))
+    yellow_block = pygame.transform.scale2x(pygame.image.load(os.path.join("graphics", "yellow.png")))
+    block_images = {"blue": blue_block, "green": green_block, "red": red_block, "violet": violet_block, "yellow": yellow_block}
+
+    field = Playfield(screen, block_images)
+    cursor = Cursor()
+    #field.fill_with_blocks()
 
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        
         field.update()
         field.draw()
+        
+        for s in cursor:
+            screen.blit(block_images[cursor.color], s.rect)
 
 
         pygame.display.flip()
--- playfield.py
+++ playfield.py
@@ -1,5 +1,4 @@
-import pygame, random
-from block import *
+import pygame, os, random
 
 LEFT_MARGIN = 8
 TOP_MARGIN = 8
@@ -7,19 +6,52 @@ NUM_ROWS = 20
 NUM_COLLUMNS = 10
 BLOCK_SIZE = 34
 
-class Playfield():
-    def __init__(self, screen):
-        self.screen = screen
-        self.blocks = pygame.sprite.Group()
+COLORS = ["blue", "green", "red", "violet", "yellow"]
 
-        blue_block = pygame.transform.scale2x(pygame.image.load(os.path.join("graphics", "blue.png")))
-        green_block = pygame.transform.scale2x(pygame.image.load(os.path.join("graphics", "green.png")))
-        red_block = pygame.transform.scale2x(pygame.image.load(os.path.join("graphics", "red.png")))
-        violet_block = pygame.transform.scale2x(pygame.image.load(os.path.join("graphics", "violet.png")))
-        yellow_block = pygame.transform.scale2x(pygame.image.load(os.path.join("graphics", "yellow.png")))
-        self.colors = ["blue", "green", "red", "violet", "yellow"]
-        self.block_images = {"blue": blue_block, "green": green_block, "red": red_block, "violet": violet_block, "yellow": yellow_block}
+
+def grid_to_pixel(x, y):
+    return (LEFT_MARGIN + x * BLOCK_SIZE, TOP_MARGIN + y * BLOCK_SIZE)
+
+def pixel_to_grid(x, y):
+    return ((x - LEFT_MARGIN) / BLOCK_SIZE, (y - TOP_MARGIN) / BLOCK_SIZE)
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self, color, x = 0, y = 0):
+        super().__init__()
+
+        self.color = color
+        self.surf = pygame.Surface((BLOCK_SIZE, BLOCK_SIZE))
+        self.rect = self.surf.get_rect().move(x, y)
         
+    
+    def move(self, x, y):
+        self.rect = self.rect.move(x, y)
+
+
+class Grid(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        # This creates a grid of NUM_ROWS by NUM_COLLUMNS
+        self.grid = [[None for i in range(NUM_ROWS)] for i in range(NUM_COLLUMNS)]
+    
+    def add_to_grid(self, block, pos_x, pos_y):
+        x, y = grid_to_pixel(pos_x, pos_y)
+        block.move(x, y)
+        self.add(block)
+        self.grid[pos_x][pos_y] = block
+    
+    def rem_from_grid(self, pos_x, pos_y):
+        block = self.grid[pos_x][pos_y]
+        self.remove(block)
+        self.grid[pos_x][pos_y] = None
+
+
+class Playfield():
+    def __init__(self, screen, images_dict):
+        self.screen = screen
+        self.blocks = Grid()
+        self.colors = COLORS
+        self.block_images = images_dict
     
     def update(self):
         for s in self.blocks:
@@ -30,17 +62,51 @@ class Playfield():
     def draw(self):
         self.screen.fill("black")
         for s in self.blocks:
-            self.screen.blit(s.surf, s.rect)
+            self.screen.blit(self.block_images[s.color], s.rect)
 
-    def add_block(self, grid_x, grid_y,color=None):
+    def add_block(self, grid_x, grid_y, color=None):
         if color is None:
             color = random.choice(self.colors)
         
-        x = LEFT_MARGIN + grid_x * BLOCK_SIZE
-        y = TOP_MARGIN + grid_y * BLOCK_SIZE
-        self.blocks.add(Block(color, self.block_images[color], x, y))
+        self.blocks.add_to_grid(Block(color), grid_x, grid_y)
 
     def fill_with_blocks(self):
         for x in range(0, NUM_COLLUMNS):
             for y in range(0, NUM_ROWS):
                 self.add_block(x, y)
+
+class Cursor(pygame.sprite.Group):
+    def __init__(self, color = None, shape = None):
+        super().__init__()
+        self.pos = 5
+        if color is not None:
+            self.color = color
+        else:
+            self.color = random.choice(COLORS)
+        if shape is not None:
+            self.shape = shape
+        else:
+            self.shape = [[None, True, None],
+                          [None, True, True],
+                          [None, True, None]]
+        
+        self.make_cursor_blocks()
+
+
+    def move(self, delta):
+        self.pos += delta
+        if self.pos < 0:
+            self.pos = 0
+        if self.pos > NUM_COLLUMNS:
+            self.pos = NUM_COLLUMNS
+    
+    def make_cursor_blocks(self):
+        for x in range(len(self.shape)):
+            for y in range(len(self.shape[x])):
+                if self.shape[x][y] is True:
+                    block = Block(self.color)
+                    pos_x, pos_y = grid_to_pixel(self.pos + x, y)
+                    block.move(pos_x,pos_y)
+                    self.shape[x][y] = block
+                    self.add(block)
+
